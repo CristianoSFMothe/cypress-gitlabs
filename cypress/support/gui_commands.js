a0cac9e4a5d696206a7137a117fcc38199a1bbeb
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-12-28 22:43:15 -0300

    :rocket: criação de projeto

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,3 +1,4 @@
+// Comando para Login
 Cypress.Commands.add('login', (
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
@@ -13,7 +14,18 @@ Cypress.Commands.add('login', (
   login()
 })
 
+// Comando para Logout
 Cypress.Commands.add('logout', () => {
   cy.get('.qa-user-avatar').click()
   cy.contains('Sign out').click()
+})
+
+// Comando para Criar Projeto
+Cypress.Commands.add('gui_createProject', project => {
+  cy.visit('/projects/new')
+
+  cy.get('#project_name').type(project.name)
+  cy.get('#project_description').type(project.description)
+  cy.get('.qa-initialize-with-readme-checkbox').check()
+  cy.contains('Create project').click()
 })
