4f1a20485011af51c71133c573b2dd2eadec0578
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-12-29 20:46:38 -0300

    :up: otimizando criando de issue

--- cypress/e2e/gui/createIssue.cy.js
+++ cypress/e2e/gui/createIssue.cy.js
@@ -13,7 +13,7 @@ describe('Create Issue', () => {
   beforeEach(() => {
     cy.api_deleteProjects();
     cy.login()
-    cy.gui_createProject(issue.project)
+    cy.api_createProject(issue.project)
   })
 
   it('successfully', () => {
