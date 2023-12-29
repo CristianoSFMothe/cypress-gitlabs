8b18b2c553c6ebfee05a51d5a247849a2f8ac2a1
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-12-28 23:10:58 -0300

    :up: Validado a sessão

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,27 +1,50 @@
 // Comando para Login
 Cypress.Commands.add('login', (
+  // Parâmetros opcionais com valores padrão das variáveis de ambiente do Cypress
   user = Cypress.env('user_name'),
   password = Cypress.env('user_password'),
+  // Opção para habilitar ou desabilitar o cache da sessão
   { cacheSession = true } = {},
 ) => {
+
+  // Função interna para realizar o processo de login
   const login = () => {
+    // Visita a página de login
     cy.visit('/users/sign_in')
 
     cy.get("[data-qa-selector='login_field']").type(user)
     cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+    // Clica no botão de login
     cy.get("[data-qa-selector='sign_in_button']").click()
   }
-
-  const options = {
-    cacheAcrossSpecs: true,
+ 
+  // Função interna para validar se o login foi bem-sucedido
+  const validate = () => {
+    // Visita a página inicial após o login
+    cy.visit('/')
+    // Verifica a localização atual para garantir que não estamos na página de login
+    cy.location('pathname', { timeout: 1000 })
+      .should('not.eq', '/users/sign_in')
   }
 
+  // Opções para o comando de sessão Cypress
+  const options = {
+    // Permite que o cache da sessão persista entre diferentes especificações
+    cacheAcrossSpecs: true, 
+    // Usa a função 'validate' para verificar se o login foi bem-sucedido
+    validate, 
+  }
+
+  // Verifica se o cache da sessão está habilitado
   if (cacheSession) {
+    // Se estiver habilitado, inicia uma sessão com o nome de usuário e a função de login
     cy.session(user, login, options)
   } else {
+    // Se o cache da sessão estiver desabilitado, apenas executa o processo de login
     login()
   }
 })
+
 
 // Comando para Logout
 Cypress.Commands.add('logout', () => {
