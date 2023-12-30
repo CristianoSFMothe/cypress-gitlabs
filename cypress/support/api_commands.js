b6756339d4ff2c070b76f4520b4aae02239d636b
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-12-29 23:10:42 -0300

    :rocket: criação de milestone

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -57,3 +57,12 @@ Cypress.Commands.add('api_createLabel', (projectId, label) => {
     headers: { Authorization: accessToken },
   })
 })
+
+Cypress.Commands.add('api_createMilestone', (projectId, milestone) => {
+  cy.request({
+    method: 'POST',
+    url: `/api/v4/projects/${projectId}/milestones`,
+    body: { title: milestone.title },
+    headers: { Authorization: accessToken },
+  })
+})
