7ebcfc0f6c38bb0e6fce5ae18863fff2bd99573c
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-12-29 22:42:35 -0300

    :up: feedback visual no teste via GUI

--- cypress/e2e/gui/createIssue.cy.js
+++ cypress/e2e/gui/createIssue.cy.js
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 
-describe('Create Issue', () => {
+const options = { env: { snapshotOnly: true } }
+
+describe('Create Issue', options, () => {
   const issue = {
     title: `issue-${faker.datatype.uuid()}`,
     description: faker.random.words(3),
--- cypress/e2e/gui/createProject.cy.js
+++ cypress/e2e/gui/createProject.cy.js
@@ -1,6 +1,8 @@
 import { faker } from "@faker-js/faker";
 
-describe("Create Project", () => {
+const options = { env: { snapshotOnly: true } }
+
+describe("Create Project", options, () => {
   beforeEach(() => {
     cy.api_deleteProjects();
     cy.login();
